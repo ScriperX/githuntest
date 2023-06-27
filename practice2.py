a4ae282727eaff1ffe4d60bd8975b8d1b4de01eb
ScriperX <137811492+ScriperX@users.noreply.github.com>
2023-06-26 22:12:10 -0500

    updated
    06/26 2212

--- practice2.py
+++ practice2.py
@@ -1,23 +1,52 @@
 import calendar
+import math
 
-def greet(name):
-    print(f'Hello {name}!')
+# def greet(name):
+#     print(f'Hello {name}!')
 
-#print(greet('Santiago'))
+# #print(greet('Santiago'))
 
-# def square(num1, num2):
-#     squarenum1 = num1**2
-#     squarenum2 = num2**2
-#     result = squarenum1 + squarenum2 
-#     print(f'The square of {num1} is {squarenum1} and for {num2} is {squarenum2}. The plus of the two results is {result}')
+# # def square(num1, num2):
+# #     squarenum1 = num1**2
+# #     squarenum2 = num2**2
+# #     result = squarenum1 + squarenum2 
+# #     print(f'The square of {num1} is {squarenum1} and for {num2} is {squarenum2}. The plus of the two results is {result}')
 
-# num1 = int(input('Enter the 1st number: '))
+# # num1 = int(input('Enter the 1st number: '))
 
-# num2 = int(input('Enter the 2nd number: '))
+# # num2 = int(input('Enter the 2nd number: '))
 
     
 
-#print(square(num1, num2))
+# #print(square(num1, num2))
 
-cal = calendar.month(1887,6)
-print(cal)
+# cal = calendar.month(1887,6)
+# #print(cal)
+
+# result = math.sqrt(49)
+# #print(result)
+
+# def minimun(num1,num2):
+    
+
+#     if num1 < num2:
+#         print(f'{num1} is less than {num2}')
+#     elif num1 == num2:
+#         print(f'{num2} and {num1} are equal')
+#     else:
+#         print(f'{num2} is less than {num1}')
+
+# num1=input('Enter the 1sr number: ')
+# num2=input('Enter the 2nd number: ')
+
+# result = minimun(num1,num2)
+# print(result)
+
+def rainning():
+    input = print('is it rainning? (yes or not)')
+    if input == 'yes':
+        print('Bring an umbrella')
+    elif input == 'no':
+        print("You don't need an umbrella today")
+        
+
